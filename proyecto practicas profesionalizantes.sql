1f4a2359f46c3864f89503179791c7e00ac217e3
Lorenzo-Sebastian-Saavedra <136624812+Lorenzo-Sebastian-Saavedra@users.noreply.github.com>
2023-10-18 10:13:31 -0300

    Update proyecto practicas profesionalizantes.sql

--- proyecto practicas profesionalizantes.sql
+++ proyecto practicas profesionalizantes.sql
@@ -1,20 +1,37 @@
--- Crear la base de datos vacaciones
+-- Código SQL para crear la base de datos y las tablas
+
 CREATE DATABASE vacaciones;
--- Asignar un usuario y una clave a la base de datos
-GRANT ALL PRIVILEGES ON vacaciones.* TO 'usuario'@'localhost' IDENTIFIED BY 'clave';
--- Seleccionar la base de datos vacaciones
 USE vacaciones;
--- Crear la tabla empleados
+
 CREATE TABLE empleados (
   id INT PRIMARY KEY AUTO_INCREMENT,
   nombre VARCHAR(50) NOT NULL,
-  apellido VARCHAR(50) NOT NULL,
-  email VARCHAR(100) UNIQUE NOT NULL,
-  dias_disponibles INT DEFAULT 15,
-  dias_tomados INT DEFAULT 0
+  password VARCHAR(50) NOT NULL,
+  fecha_ingreso DATE NOT NULL,
+  vacaciones_disponibles INT NOT NULL
 );
--- Insertar algunos registros de ejemplo
-INSERT INTO empleados (nombre, apellido, email) VALUES
-('Juan', 'Perez', 'juan.perez@empresa.com'),
-('María', 'Garcia', 'maria.garcia@empresa.com'),
-('Pedro', 'Lopez', 'pedro.lopez@empresa.com');
+
+CREATE TABLE solicitudes (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  id_empleado INT NOT NULL,
+  fecha_inicio DATE NOT NULL,
+  fecha_fin DATE NOT NULL,
+  estado VARCHAR(10) NOT NULL,
+  comentario VARCHAR(100),
+  FOREIGN KEY (id_empleado) REFERENCES empleados(id)
+);
+
+-- Código SQL para insertar algunos datos de ejemplo
+
+INSERT INTO empleados (nombre, password, fecha_ingreso, vacaciones_disponibles) VALUES
+('Juan', '1234', '2020-01-01', 15),
+('Ana', '5678', '2019-05-01', 20),
+('Pedro', 'abcd', '2021-03-01', 10),
+('Laura', 'efgh', '2020-07-01', 12),
+('Carlos', 'admin', '2018-01-01', 25); -- Carlos es el administrador o jefe
+
+INSERT INTO solicitudes (id_empleado, fecha_inicio, fecha_fin, estado, comentario) VALUES
+(1, '2023-11-01', '2023-11-05', 'pendiente', NULL),
+(2, '2023-10-15', '2023-10-20', 'aprobada', 'Disfruta tus vacaciones'),
+(3, '2023-12-01', '2023-12-10', 'rechazada', 'No hay suficiente personal'),
+(4, '2023-11-10', '2023-11-15', 'pendiente', NULL);
